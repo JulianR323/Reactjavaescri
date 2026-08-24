c77ce4b79f0fe57f173d425101d121d4c498caff
Julian Riano <rianojulian63@gmail.com>
2026-08-24 08:54:13 -0500

    first commit

---
+++ tienda-react/src/practica-js.js
@@ -0,0 +1,52 @@
+/* console.log("JavaScript listo para practicar");*/
+/*function calcularTotal(precio, cantidad) {
+    const total = precio * cantidad;
+    return total;
+}
+const resultado = calcularTotal(50000, 3);
+console.log(resultado);*/
+
+/*function suma(a, b) {
+    const suma = a + b;
+    return suma
+}
+function resta(a, b){
+    const resta = a - b;
+    return resta
+}
+function multi(a, b){
+    const multi = a * b;
+    return multi
+}
+function div(a, b){
+    const div = a / b;
+    return div
+}
+function calcularPromedio(nota1,nota2,nota3){
+    const promedio = nota1 + nota2 + nota3;
+    const calculo = promedio / 3
+    return calculo
+}
+console.log("Suma:", suma(2,3));
+console.log("Resta:", resta(8,3));
+console.log("Multi:", multi(9,9));
+console.log("Division:", div(4,2));
+console.log("Promedio:", calcularPromedio(4.0,3.5,5.0));*/
+
+/*Console log muestra el mensaje en la consola mientras que return devuelve un valor para que el resto del
+codigo pueda usarlo */
+
+/*
+
+Sin funcion flecha
+function aplicarIva(precio) {
+return precio * 1.19;
+}
+
+Funcion flecha
+const aplicarIva = (precio) => {
+return precio * 1.19;
+};
+
+Acortada con funcion tipo flecha*/
+const aplicarIva = precio => precio * 1.19;
