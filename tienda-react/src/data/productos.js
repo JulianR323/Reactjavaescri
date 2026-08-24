4ed7ac31f3ac260190393a25b5dea62466a36967
Julian Riano <rianojulian63@gmail.com>
2026-08-24 10:55:43 -0500

    creacion carpeta data y archivo productos.js

---
+++ tienda-react/src/data/productos.js
@@ -0,0 +1,12 @@
+const productos = [
+ { id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+ { id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+ { id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 },
+  { id: 4, nombre: 'Audifonos', precio: 15000, categoria: 'Perifericos', stock: 3 },
+   { id: 5, nombre: 'USB de Almacenamiento', precio: 25000, categoria: 'Almacenamiento', stock: 6 },
+    { id: 6, nombre: 'Mousepad', precio: 30000, categoria: 'Perifericos', stock: 7 },
+    { id: 7, nombre: 'Mesa', precio: 350000, categoria: 'Muebles', stock: 3 },
+    { id: 8, nombre: 'Silla', precio: 10000, categoria: 'Muebles', stock: 0 },
+    { id: 9, nombre: 'Ventilador', precio: 20000, categoria: 'Electrodomesticos', stock: 5 },
+    { id: 10, nombre: 'Camara', precio: 120000, categoria: 'Perifericos', stock: 1 },
+];
