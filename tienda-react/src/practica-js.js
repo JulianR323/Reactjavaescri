6f06fe7c62a58e16fefbda9b2e28111e61f4e083
Julian Riano <rianojulian63@gmail.com>
2026-08-24 10:11:40 -0500

    cambios arreglos

--- tienda-react/src/practica-js.js
+++ tienda-react/src/practica-js.js
@@ -87,8 +87,8 @@ const productos = [
    { id: 5, nombre: 'USB de Almacenamiento', precio: 25000, categoria: 'Almacenamiento', stock: 6 },
     { id: 6, nombre: 'Mousepad', precio: 30000, categoria: 'Perifericos', stock: 7 },
     { id: 7, nombre: 'Mesa', precio: 350000, categoria: 'Muebles', stock: 3 },
-    { id: 8, nombre: 'Silla', precio: 10000, categoria: 'Muebles', stock: 9 },
+    { id: 8, nombre: 'Silla', precio: 10000, categoria: 'Muebles', stock: 0 },
     { id: 9, nombre: 'Ventilador', precio: 20000, categoria: 'Electrodomesticos', stock: 5 },
-    { id: 10, nombre: 'Camara', precio: 50000, categoria: 'Perifericos', stock: 1 },
+    { id: 10, nombre: 'Camara', precio: 120000, categoria: 'Perifericos', stock: 1 },
 ];
 console.log(productos);
